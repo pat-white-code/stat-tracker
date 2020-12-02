0e930d455134602eb1c8c2e212fd89b28a4bf94e
pat-white-code <pat.k.white@gmail.com>
2020-12-01 21:01:52 -0600

    pulls in csv data, parses

--- src/components/CaloriesBurned.js
+++ src/components/CaloriesBurned.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BarChart from './data-viz/BarChart';
+import * as d3 from 'd3';
 
-const data = [
-  {day: 1,
-  burned: 4500,
-  consumed: 2000
-  },
-  {day: 2,
-  burned: 3500,
-  consumed: 2000
-  },
-  {day: 3,
-  burned: 1500,
-  consumed: 2000
-  },
-  {day: 4,
-  burned: 2000,
-  consumed: 2000
-  },
-  {day: 5,
-  burned: 2500,
-  consumed: 2000
-  }
-]
+// const data = [
+//   {day: 1,
+//   burned: 4500,
+//   consumed: 2000
+//   },
+//   {day: 2,
+//   burned: 3500,
+//   consumed: 2000
+//   },
+//   {day: 3,
+//   burned: 1500,
+//   consumed: 2000
+//   },
+//   {day: 4,
+//   burned: 2000,
+//   consumed: 2000
+//   },
+//   {day: 5,
+//   burned: 2500,
+//   consumed: 2000
+//   }
+// ]
 
 const CaloriesBurned = props => {
+  const [caloriesBurned, setCaloriesBurned] = useState([]);
+  let data;
+  useEffect(() => {
+    d3.csv('/data/calories_burned_jan.csv').then(d => {
+      setCaloriesBurned(d)
+    })
+  }, [])
+
   const {calorieGoal} = props
   return (
     <div>
@@ -35,7 +44,7 @@ const CaloriesBurned = props => {
             y={50}
             height={300} 
             width={500}
-            data={data}
+            data={caloriesBurned}
             goal={calorieGoal}
           />
         </svg>
