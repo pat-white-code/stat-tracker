224fd152395e9645e17d138bda35a6c042177328
pat-white-code <pat.k.white@gmail.com>
2020-12-03 23:11:59 -0600

    plays with adding functionality

--- src/components/NutriPie.js
+++ src/components/NutriPie.js
@@ -5,11 +5,38 @@ import DonutChart from './data-viz/DonutChart';
 const NutriComp = () => {
   const [nutrition, setNutrition] = useState(null);
   useEffect(()=> {
-    d3.csv('/data/gifts-likely.csv', d => ({
-      stuffer: d.gift, percent: +d.percent})
+    d3.csv('/data/nutrition.csv', d => ({
+      nutrition: d.nutrition, calories: +d.calories})
     ).then(d => setNutrition(d))
-  }, [!nutrition])
+  }, [])
 
+  const foods = {
+    burger: {
+      fat: 10,
+      carbs: 20,
+      protein: 30
+    }
+  }
+
+  const handleClick = e => {
+    console.log(e.target.value);
+    const food = foods[e.target.value];
+    const stateModel = [
+      {
+        nutrition: 'fat',
+        calories: nutrition[0].calories + food["fat"]
+      },
+      {
+        nutrition: 'carbs',
+        calories: nutrition[1].calories + food["carbs"]
+      },
+      {
+        nutrition: 'protein',
+        calories: nutrition[2].calories + food["protein"]
+      },
+    ];
+    setNutrition(stateModel)
+  }
   return(
     <div>
       <h1>A breakdown of your nutrition</h1>
@@ -17,6 +44,11 @@ const NutriComp = () => {
         <DonutChart height={300} width={300} data={nutrition} />
         {/* <DonutChart x={250} y={250} r={100} data={nutrition} /> */}
       </svg>
+      <button value="burger" onClick={handleClick}>Add Burger</button>
+      <button>Chicken</button>
+      <button>Pizza</button>
+      <button>Salad</button>
+      <button>Oatmeal</button>
     </div>
   )
 }
